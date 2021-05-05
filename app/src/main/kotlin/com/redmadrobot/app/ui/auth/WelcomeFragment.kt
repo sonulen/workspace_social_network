1cbe6b7f4efdbbb8d83662060f357bd3334e23fa
sonulen <sonulen@gmail.com>
2021-05-05 16:18:15 +0300

    [Review] Перевел экран Welcome на использование делегата viewBinding

--- app/src/main/kotlin/com/redmadrobot/app/ui/auth/WelcomeFragment.kt
+++ app/src/main/kotlin/com/redmadrobot/app/ui/auth/WelcomeFragment.kt
@@ -1,41 +1,31 @@
 package com.redmadrobot.app.ui.auth
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.redmadrobot.app.R
 import com.redmadrobot.app.databinding.WelcomeFragmentBinding
 import com.redmadrobot.app.ui.base.fragment.BaseFragment
+import com.redmadrobot.extensions.viewbinding.viewBinding
 
 class WelcomeFragment : BaseFragment(R.layout.welcome_fragment) {
-    private var _binding: WelcomeFragmentBinding? = null
-    private val binding get() = _binding!!
+    private val binding: WelcomeFragmentBinding by viewBinding()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = WelcomeFragmentBinding.inflate(inflater, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         registerButtonClickListeners()
-        return binding.root
     }
 
     private fun registerButtonClickListeners() {
         val navController = findNavController(this)
-        binding.buttonLogin.setOnClickListener {
-            navController.navigate(R.id.toLoginFragment)
-        }
+        with(binding) {
+            buttonLogin.setOnClickListener {
+                navController.navigate(R.id.toLoginFragment)
+            }
 
-        binding.btnRegisterByMail.setOnClickListener {
-            navController.navigate(R.id.toRegisterFragment)
+            btnRegisterByMail.setOnClickListener {
+                navController.navigate(R.id.toRegisterFragment)
+            }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
