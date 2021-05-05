b9fd46eda764f08207dbab9eb40747e4275e8eb8
sonulen <sonulen@gmail.com>
2021-05-05 16:55:15 +0300

    [Refactoring] Почистил экран ProfileFragment

--- app/src/main/kotlin/com/redmadrobot/app/ui/feed/ProfileFragment.kt
+++ app/src/main/kotlin/com/redmadrobot/app/ui/feed/ProfileFragment.kt
@@ -7,22 +7,17 @@ import android.view.ViewGroup
 import com.redmadrobot.app.R
 import com.redmadrobot.app.databinding.ProfileFragmentBinding
 import com.redmadrobot.app.ui.base.fragment.BaseFragment
+import com.redmadrobot.extensions.viewbinding.inflateViewBinding
 
 class ProfileFragment : BaseFragment(R.layout.feed_fragment) {
-    private var _binding: ProfileFragmentBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: ProfileFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = ProfileFragmentBinding.inflate(inflater, container, false)
+        binding = inflater.inflateViewBinding(container, attachToRoot = false)
         return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
