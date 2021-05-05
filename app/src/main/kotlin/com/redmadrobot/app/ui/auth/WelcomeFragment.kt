3496ee82fbe31b59e5a19761d7d6adc8714baaa3
sonulen <sonulen@gmail.com>
2021-05-05 05:13:46 +0300

    [Refactoring] Перевел экран Welcome на ViewBinding

--- app/src/main/kotlin/com/redmadrobot/app/ui/auth/WelcomeFragment.kt
+++ app/src/main/kotlin/com/redmadrobot/app/ui/auth/WelcomeFragment.kt
@@ -4,30 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.redmadrobot.app.R
+import com.redmadrobot.app.databinding.WelcomeFragmentBinding
 import com.redmadrobot.app.ui.base.fragment.BaseFragment
 
 class WelcomeFragment : BaseFragment(R.layout.welcome_fragment) {
+    private var _binding: WelcomeFragmentBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val view = inflater.inflate(R.layout.welcome_fragment, container, false)
-        registerButtonClickListeners(view)
-        return view
+        _binding = WelcomeFragmentBinding.inflate(inflater, container, false)
+        registerButtonClickListeners()
+        return binding.root
     }
 
-    private fun registerButtonClickListeners(view: View) {
+    private fun registerButtonClickListeners() {
         val navController = findNavController(this)
-        view.findViewById<Button>(R.id.button_login).setOnClickListener {
+        binding.buttonLogin.setOnClickListener {
             navController.navigate(R.id.toLoginFragment)
         }
 
-        view.findViewById<Button>(R.id.btn_register_by_mail).setOnClickListener {
+        binding.btnRegisterByMail.setOnClickListener {
             navController.navigate(R.id.toRegisterFragment)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
