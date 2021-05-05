3ab791f2ccf8f4405e94a056192ad3489839ffe9
sonulen <sonulen@gmail.com>
2021-05-05 05:09:22 +0300

    [Refactoring] Перевел экран Feed на ViewBinding

--- app/src/main/kotlin/com/redmadrobot/app/ui/feed/FeedFragment.kt
+++ app/src/main/kotlin/com/redmadrobot/app/ui/feed/FeedFragment.kt
@@ -4,37 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import com.redmadrobot.app.R
+import com.redmadrobot.app.databinding.FeedFragmentBinding
 import com.redmadrobot.app.ui.base.fragment.BaseFragment
 
 class FeedFragment : BaseFragment(R.layout.feed_fragment) {
+    private var _binding: FeedFragmentBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val view = inflater.inflate(
-            R.layout.feed_fragment,
-            container,
-            false
-        )
+        _binding = FeedFragmentBinding.inflate(inflater, container, false)
 
-        registerButtonClickListeners(view)
+        registerButtonClickListeners()
 
-        return view
+        return binding.root
     }
 
-    private fun registerButtonClickListeners(view: View) {
-        view.findViewById<Button>(R.id.btn_find_friends).setOnClickListener {
-            Toast.makeText(view.context, "Извини, но все разошлись!", Toast.LENGTH_SHORT).show()
+    private fun registerButtonClickListeners() {
+        binding.btnFindFriends.setOnClickListener {
+            Toast.makeText(context, "Извини, но все разошлись!", Toast.LENGTH_SHORT).show()
         }
 
         val navController = NavHostFragment.findNavController(this)
-        view.findViewById<Button>(R.id.btn_deauth).setOnClickListener {
+        binding.btnDeauth.setOnClickListener {
             navController.navigate(R.id.toWelcomeFragment)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
