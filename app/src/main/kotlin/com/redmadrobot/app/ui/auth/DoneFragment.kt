ccb0975ce406e80981fa494022ebb1e77ee49e93
sonulen <sonulen@gmail.com>
2021-05-05 05:11:28 +0300

    [Refactoring] Перевел экран Done на ViewBinding

--- app/src/main/kotlin/com/redmadrobot/app/ui/auth/DoneFragment.kt
+++ app/src/main/kotlin/com/redmadrobot/app/ui/auth/DoneFragment.kt
@@ -4,26 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.redmadrobot.app.R
+import com.redmadrobot.app.databinding.DoneFragmentBinding
 import com.redmadrobot.app.ui.base.fragment.BaseFragment
 
 class DoneFragment : BaseFragment(R.layout.done_fragment) {
+    private var _binding: DoneFragmentBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val view = inflater.inflate(R.layout.done_fragment, container, false)
-        registerButtonClickListeners(view)
-        return view
+        _binding = DoneFragmentBinding.inflate(inflater, container, false)
+        registerButtonClickListeners()
+        return binding.root
     }
 
-    private fun registerButtonClickListeners(view: View) {
+    private fun registerButtonClickListeners() {
         val navController = findNavController(this)
-        view.findViewById<Button>(R.id.btn_go_to_feed).setOnClickListener {
+        binding.btnGoToFeed.setOnClickListener {
             navController.navigate(R.id.toFeedFragment)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
