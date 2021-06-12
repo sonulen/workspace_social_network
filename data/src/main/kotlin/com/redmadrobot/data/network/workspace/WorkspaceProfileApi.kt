a173d37a78572ba1f330ca649e61aeeaf3bd74f4
sonulen <sonulen@gmail.com>
2021-06-12 18:07:47 +0300

    [Сеть] Исправил ошибки в возвращаемых значения Api при 204

--- data/src/main/kotlin/com/redmadrobot/data/network/workspace/WorkspaceProfileApi.kt
+++ data/src/main/kotlin/com/redmadrobot/data/network/workspace/WorkspaceProfileApi.kt
@@ -2,6 +2,7 @@ package com.redmadrobot.data.network.workspace
 
 import com.redmadrobot.data.entity.api.response.NetworkEntityPosts
 import com.redmadrobot.data.network.NetworkRouter
+import retrofit2.Response
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -13,10 +14,10 @@ interface WorkspaceProfileApi {
     suspend fun feedGet(): NetworkEntityPosts
 
     @POST(NetworkRouter.FEED_LIKE)
-    suspend fun feedLike(@Path("id") id: String)
+    suspend fun feedLike(@Path("id") id: String): Response<Unit>
 
     @DELETE(NetworkRouter.FEED_DELETE_LIKE)
-    suspend fun feedDeleteLike(@Path("id") id: String)
+    suspend fun feedDeleteLike(@Path("id") id: String): Response<Unit>
 
     @GET(NetworkRouter.FEED_GET_FAVORITE)
     suspend fun feedGetFavorite(): NetworkEntityPosts
