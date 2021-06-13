4929e0dc2834922dfc7fe3177ea32149fc3fe97a
sonulen <sonulen@gmail.com>
2021-06-14 00:14:08 +0300

    [Безопасность] Добавил правило на проверку isDebuggerConnected()

--- app/src/main/kotlin/com/redmadrobot/app/ui/MainViewModel.kt
+++ app/src/main/kotlin/com/redmadrobot/app/ui/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.redmadrobot.app.ui
 
 import android.content.Context
+import android.os.Debug
 import androidx.lifecycle.viewModelScope
 import com.redmadrobot.app.BuildConfig
 import com.redmadrobot.app.R
@@ -42,7 +43,7 @@ class MainViewModel @Inject constructor(
                 context.getString(R.string.root_env_message)
             ))
         }
-        if (BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG || Debug.isDebuggerConnected()) {
             eventsQueue.offerEvent(EventError(
                 context.getString(R.string.debug_mode_message)
             ))
